a7e66e63caee1cda4abf7d1fdef36d08176960d1
Alan <ahuang7840@gmail.com>
2024-12-24 10:26:27 -0500

    projects should open on a different tab

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -49,6 +49,8 @@ export function ProjectCard({
             <Link
                 href={href || "#"}
                 className={cn("block cursor-pointer", className)}
+                target="_blank" 
+                rel="noopener noreferrer" 
             >
                 {video && (
                     <video
@@ -57,7 +59,7 @@ export function ProjectCard({
                         loop
                         muted
                         playsInline
-                        className="pointer-events-none mx-auto h-40 w-full object-cover object-top" // needed because random black line at bottom of video
+                        className="pointer-events-none mx-auto h-40 w-full object-cover object-top"
                     />
                 )}
                 {image && (
@@ -104,11 +106,13 @@ export function ProjectCard({
                 {links && links.length > 0 && (
                     <div className="flex flex-row flex-wrap items-start gap-1">
                         {links?.map((link, idx) => (
-                            <Link href={link?.href} key={idx} target="_blank">
-                                <Badge
-                                    key={idx}
-                                    className="flex gap-2 px-2 py-1 text-[10px]"
-                                >
+                            <Link
+                                href={link?.href}
+                                key={idx}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <Badge className="flex gap-2 px-2 py-1 text-[10px]">
                                     {link.icon}
                                     {link.type}
                                 </Badge>
